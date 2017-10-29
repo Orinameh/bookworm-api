7be3efde60c3fc591bf67d5290f71515e14cb57b
Orinameh <memefacto@yahoo.com>
2017-10-29 07:05:18 +0100

    added user sign up

--- src/index.js
+++ src/index.js
@@ -3,17 +3,22 @@ import path from "path";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import Promise from "bluebird";
 
 import auth from './routes/auth';
+import users from './routes/users';
+
 
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 
-mongoose.Promise = global.Promise;
+mongoose.Promise = Promise
 mongoose.connect(process.env.MONGODB_URL, {useMongoClient: true});
 
 app.use("/api/auth", auth);
+app.use("/api/users", users);
+
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
