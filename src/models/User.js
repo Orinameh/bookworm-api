bc879797493add447faf845ebea7de02653de19b
Orinameh <memefacto@yahoo.com>
2017-11-17 16:47:03 +0100

    ...

--- src/models/User.js
+++ src/models/User.js
@@ -2,12 +2,16 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import uniqueValidator from "mongoose-unique-validator";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // TODO: add uniqueness and email validation to email field
 const schema = new mongoose.Schema({
   email: { type: String, required: true, lowercase:true, index: true, unique: true},
   passwordHash: { type: String, required: true },
-  confirmed: {type: Boolean, default: false}
+  confirmed: {type: Boolean, default: false},
+  confirmationToken: { type: String, default: ""}
 }, {timestamps: true});
 
 // adds a method to the schema
@@ -17,6 +21,14 @@ schema.methods.isValidPassword = function isValidPassword(password) {
 
 schema.methods.setPassword = function setPassword(password) {
   this.passwordHash = bcrypt.hashSync(password, 10);
+}
+
+schema.methods.setConfirmationToken = function setConfirmationToken() {
+  this.confirmationToken = this.generateJWT();
+}
+
+schema.methods.generateConfirmationUrl = function generateConfirmationUrl() {
+  return `${process.env.HOST}/confirmation/${this.confirmationToken}`
 }
 
 schema.methods.generateJWT = function generateJWT() {
